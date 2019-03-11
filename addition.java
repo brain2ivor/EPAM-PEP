4527ffb789d8955f7e600ee474a8432af4bfa345
brain2ivor <36896299+brain2ivor@users.noreply.github.com>
2019-03-12 02:14:44 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		      int a,b,c;
+		 
+		      System.out.println("Enter two integers");
+		      Scanner in = new Scanner(System.in);
+		     
+		      a = in.nextInt();
+		      b = in.nextInt();
+		      c = a + b;
+		     
+		      System.out.println("Sum = " + c);
+		   }
+		
+	
+
+}
